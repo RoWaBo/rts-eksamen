ccdf88ada79f6381f753b2b3bcc0781cdad4162f
Robert <u1501851@rts-365.dk>
2022-03-15 11:38:00 +0100

    added background assets and Home and Hero

---
+++ src/components/Hero.js
@@ -0,0 +1,9 @@
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react'
+
+const Hero = () => {
+	const heroImages = ['header_bg_1.jpg', 'header_bg_2.jpg']
+	return <section></section>
+}
+
+export default Hero
