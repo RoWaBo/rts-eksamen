8856a6f44b524c54156bd9ee5d2d3a295b6acc5c
Robert <u1501851@rts-365.dk>
2022-03-15 11:50:03 +0100

    All assets added to public folder

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -3,7 +3,21 @@ import { css } from '@emotion/react'
 
 const Hero = () => {
 	const heroImages = ['header_bg_1.jpg', 'header_bg_2.jpg']
-	return <section></section>
+	function getRandomInt(min, max) {
+		min = Math.ceil(min)
+		max = Math.floor(max)
+		return Math.floor(Math.random() * (max - min) + min) //The maximum is exclusive and the minimum is inclusive
+	}
+	// === STYLE ===
+	const sectionStyle = css`
+		width: 100vw;
+		height: 100vh;
+		background-image: url('./assets/bg/${heroImages[getRandomInt(0, 2)]}');
+		background-position: center;
+		background-repeat: no-repeat;
+		background-size: cover;
+	`
+	return <section css={sectionStyle}></section>
 }
 
 export default Hero
