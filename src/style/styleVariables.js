0a9f5e2ed6167183cffcad710f75f87fff779681
Robert <u1501851@rts-365.dk>
2022-03-15 12:18:01 +0100

    colors added to styleVariables

--- src/style/styleVariables.js
+++ src/style/styleVariables.js
@@ -1,3 +1,8 @@
-export const color = {}
+export const color = {
+	black: '#030203',
+	pink: '#ff2a70',
+	grey: '#e0e0e0',
+	white: '#ffffff',
+}
 export const spacing = { gutter: '1rem' }
 export const fontSize = {}
