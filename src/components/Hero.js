c304c804abc037a15091fe591aca924a3db7dd77
Robert <u1501851@rts-365.dk>
2022-03-15 12:58:38 +0100

    Created Hero animations

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,6 +1,7 @@
 /** @jsxImportSource @emotion/react */
 import { css } from '@emotion/react'
 import { color } from '../style/styleVariables'
+import { motion } from 'framer-motion'
 
 const Hero = () => {
 	const heroImages = ['header_bg_1.jpg', 'header_bg_2.jpg']
@@ -38,16 +39,57 @@ const Hero = () => {
 			padding: 0 3rem;
 		}
 	`
+	// === ANIMATION VARIANTS ===
+	const logoAnimation = {
+		initial: {
+			rotateX: 95,
+			opacity: 0,
+		},
+		animate: {
+			rotateX: 0,
+			opacity: 1,
+			transition: {
+				duration: 1.5,
+				ease: 'easeInOut',
+			},
+		},
+	}
+	const sloganAnimation = {
+		initial: {
+			opacity: 0,
+			y: -50,
+		},
+		animate: {
+			opacity: 1,
+			y: 0,
+			transition: {
+				delay: 1,
+				duration: 1,
+				ease: 'easeInOut',
+			},
+		},
+	}
 	return (
 		<section css={sectionStyle}>
 			<div>
-				<img src='./assets/icon/Logo.svg' alt='night club logo' />
-				<h2 className='slogan'>Have a good time</h2>
-				<img
-					className='bottomLine'
-					src='./assets/bottom_line2.png'
+				<motion.img
+					src='./assets/icon/Logo.svg'
 					alt='night club logo'
+					variants={logoAnimation}
+					initial='initial'
+					animate='animate'
 				/>
+				<motion.div
+					variants={sloganAnimation}
+					initial='initial'
+					animate='animate'>
+					<h2 className='slogan'>Have a good time</h2>
+					<img
+						className='bottomLine'
+						src='./assets/bottom_line2.png'
+						alt='night club logo'
+					/>
+				</motion.div>
 			</div>
 		</section>
 	)
