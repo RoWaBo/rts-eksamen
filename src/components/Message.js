664d244dd7a2584be07832c16ee09b6a379d1eb9
Robert <u1501851@rts-365.dk>
2022-03-16 18:37:02 +0100

    Created Message for errors and succes

---
+++ src/components/Message.js
@@ -0,0 +1,31 @@
+import { css } from '@emotion/react'
+/** @jsxImportSource @emotion/react */
+import { motion } from 'framer-motion'
+
+const Message = ({ children, error, succes, ...props }) => {
+	const messageStyle = css`
+		font-size: 14px;
+		color: white;
+	`
+	const containerStyle = css`
+		${error && 'background: rgb(239 68 68 / 60%);'}
+		border-radius: 10px;
+		padding: 0.5rem 1rem;
+		max-width: fit-content;
+	`
+
+	return (
+		<motion.div
+			css={containerStyle}
+			{...props}
+			key={'message'}
+			initial={{ opacity: 0, y: 20 }}
+			animate={{ opacity: 1, y: 0 }}
+			exit={{ opacity: 0, y: 20 }}
+			transition={{ duration: 0.5 }}>
+			<p css={messageStyle}>{children}</p>
+		</motion.div>
+	)
+}
+
+export default Message
