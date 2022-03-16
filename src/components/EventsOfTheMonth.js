34d53d91b4efc77f4eb6835522772c32ce0c10cb
Robert <u1501851@rts-365.dk>
2022-03-16 08:49:39 +0100

    Created swiper

--- src/components/EventsOfTheMonth.js
+++ src/components/EventsOfTheMonth.js
@@ -5,6 +5,12 @@ import Article from './Article'
 /** @jsxImportSource @emotion/react */
 import { css } from '@emotion/react'
 import { color } from '../style/styleVariables'
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react'
+import { Pagination, Autoplay } from 'swiper'
+// Import Swiper styles
+import 'swiper/css'
+import 'swiper/css/pagination'
 
 const EventsOfTheMonth = () => {
 	const [events, setEvents] = useState()
@@ -42,27 +48,71 @@ const EventsOfTheMonth = () => {
 	`
 	const timeBarStyle = css`
 		width: 700px;
-		height: 25px;
+		padding: 0.5rem;
 		background: ${color.pink};
+	`
+	const myPaginationStyle = css`
+		text-align: center;
+		transition: 0.3s opacity;
+		transform: translate3d(0, 0, 0);
+		width: 100%;
+		margin-top: 2.5rem;
+
+		& > * + * {
+			margin-left: 0.5rem;
+		}
+
+		.swiper-pagination-bullet {
+			width: 20px;
+			height: 20px;
+			border-radius: 0;
+			background: ${color.grey};
+			opacity: 1;
+		}
+		.swiper-pagination-bullet-active {
+			background: ${color.pink};
+		}
 	`
 	return (
 		<Article
 			heading='events of the month'
-			backgroundImage='./assets/content-img/gallery7_big.jpg'>
-			{events?.map((event) => (
-				<>
-					<motion.section
-						key={event.id}
-						css={eventStyle}
-						style={{
-							backgroundImage: `url(${event.asset.url})`,
-						}}></motion.section>
-					<div css={timeBarStyle}>
-						{dateConverter(event.date)}
-						{event.location}
-					</div>
-				</>
-			))}
+			backgroundImage='./assets/content-img/gallery7_big.jpg'
+			centerContent>
+			<Swiper
+				slidesPerView={2}
+				// spaceBetween={30}
+				slidesPerGroup={2}
+				loop={true}
+				// loopFillGroupWithBlank={true}
+				pagination={{
+					el: '.my-pagination',
+					clickable: true,
+					renderBullet: (index, className) => {
+						return '<span class="' + className + '">' + '</span>'
+					},
+				}}
+				// autoplay={{
+				// 	delay: 5000,
+				// 	disableOnInteraction: true,
+				// }}
+				navigation={true}
+				modules={[Pagination, Autoplay]}
+				className='mySwiper'>
+				{events?.map((event) => (
+					<SwiperSlide key={event.id}>
+						<motion.div
+							css={eventStyle}
+							style={{
+								backgroundImage: `url(${event.asset.url})`,
+							}}></motion.div>
+						<div css={timeBarStyle}>
+							{dateConverter(event.date)}
+							{event.location}
+						</div>
+					</SwiperSlide>
+				))}
+			</Swiper>
+			<div className='my-pagination' css={myPaginationStyle} />
 		</Article>
 	)
 }
---
+++ src/components/Swiper.js
@@ -0,0 +1,37 @@
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react'
+import { Pagination } from 'swiper'
+// Import Swiper styles
+import 'swiper/css'
+import 'swiper/css/pagination'
+
+const Swiper = () => {
+	return (
+		<>
+			<Swiper
+				slidesPerView={3}
+				spaceBetween={30}
+				slidesPerGroup={3}
+				loop={true}
+				loopFillGroupWithBlank={true}
+				pagination={{
+					clickable: true,
+				}}
+				navigation={true}
+				modules={[Pagination]}
+				className='mySwiper'>
+				<SwiperSlide>Slide 1</SwiperSlide>
+				<SwiperSlide>Slide 2</SwiperSlide>
+				<SwiperSlide>Slide 3</SwiperSlide>
+				<SwiperSlide>Slide 4</SwiperSlide>
+				<SwiperSlide>Slide 5</SwiperSlide>
+				<SwiperSlide>Slide 6</SwiperSlide>
+				<SwiperSlide>Slide 7</SwiperSlide>
+				<SwiperSlide>Slide 8</SwiperSlide>
+				<SwiperSlide>Slide 9</SwiperSlide>
+			</Swiper>
+		</>
+	)
+}
+
+export default Swiper
