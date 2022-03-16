8e6ce9ef1f6887dd2e7a02cd7c90070097316247
Robert <u1501851@rts-365.dk>
2022-03-16 14:06:48 +0100

    Clicked image is now shown in lightbox with swiperControl

--- src/components/NightClubGallery.js
+++ src/components/NightClubGallery.js
@@ -5,10 +5,21 @@ import axios from 'axios'
 import { css } from '@emotion/react'
 import { motion, AnimatePresence } from 'framer-motion'
 import Overlay from './Overlay'
+import { AiOutlineCloseSquare } from 'react-icons/ai'
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react'
+import { Navigation } from 'swiper'
+// Import Swiper styles
+import 'swiper/css'
+import 'swiper/css/navigation'
+import { color } from '../style/styleVariables'
 
 const NightClubGallery = () => {
 	const [gallery, setGallery] = useState()
 	const [hoverItemIndex, setHoverItemIndex] = useState(null)
+	const [lightboxIsVisible, setLightboxIsVisible] = useState()
+	const [swiperControl, setSwiperControl] = useState()
+	const [clickedImageIndex, setClickedImageIndex] = useState()
 
 	useEffect(() => {
 		if (gallery) return
@@ -17,6 +28,15 @@ const NightClubGallery = () => {
 			setGallery(data)
 		})()
 	}, [gallery])
+
+	useEffect(() => {
+		swiperControl && swiperControl.slideTo(clickedImageIndex, 0)
+	}, [swiperControl, clickedImageIndex])
+
+	const enableLightBox = (i) => {
+		setLightboxIsVisible(true)
+		setClickedImageIndex(i)
+	}
 
 	// === STYLE ===
 	const imageStyle = css`
@@ -32,8 +52,48 @@ const NightClubGallery = () => {
 		display: flex;
 		flex-wrap: wrap;
 	`
-	const overlayStyle = css``
+	const lightboxStyle = css`
+		position: fixed;
+		top: 0;
+		bottom: 0;
+		left: 0;
+		right: 0;
+		background: rgba(3, 2, 3, 0.8);
+		z-index: 5;
+	`
+	const closeIconStyle = css`
+		width: 50px;
+		height: 50px;
+		color: ${color.grey};
+		position: fixed;
+		top: 20px;
+		right: 30px;
+		z-index: 10;
+		cursor: pointer;
+	`
+	const swiperStyle = css`
+		opacity: ${lightboxIsVisible ? '1' : '0'};
+		position: fixed;
+		top: 0;
+		bottom: 0;
+		left: 0;
+		right: 0;
+		z-index: 6;
+		.swiper-slide {
+			display: flex;
+			justify-content: center;
+			align-items: center;
+		}
+		/* .sliderImage {
 
+		} */
+		/* .swiper-button-prev {
+			
+		}
+		.swiper-button-next {
+			
+		} */
+	`
 	// === ANIMATIONS ===
 	const galleryContainerAnimation = {
 		initial: {
@@ -48,7 +108,6 @@ const NightClubGallery = () => {
 	}
 	return (
 		<>
-			<motion.div css={overlayStyle} />
 			<Article heading='Night club gallery' centerContent>
 				<motion.div
 					css={galleryContainerStyle}
@@ -63,7 +122,8 @@ const NightClubGallery = () => {
 								backgroundImage: `url(${image.asset.url})`,
 							}}
 							onHoverStart={() => setHoverItemIndex(i)}
-							onHoverEnd={() => setHoverItemIndex(null)}>
+							onHoverEnd={() => setHoverItemIndex(null)}
+							onTap={() => enableLightBox(i)}>
 							<AnimatePresence>
 								{hoverItemIndex === i && (
 									<Overlay key='overlay' opacity={0.7} smallTriangles />
@@ -73,6 +133,34 @@ const NightClubGallery = () => {
 					))}
 				</motion.div>
 			</Article>
+
+			{lightboxIsVisible && (
+				<>
+					<motion.div css={lightboxStyle} />
+					<AiOutlineCloseSquare
+						css={closeIconStyle}
+						onClick={() => setLightboxIsVisible(false)}
+					/>
+					<Swiper
+						onSwiper={setSwiperControl}
+						css={swiperStyle}
+						navigation={true}
+						modules={[Navigation]}
+						// loop
+						centeredSlides
+						className='mySwiper'>
+						{gallery?.map((image, i) => (
+							<SwiperSlide key={i}>
+								<img
+									className='sliderImage'
+									src={image.asset.url}
+									alt={image.description}
+								/>
+							</SwiperSlide>
+						))}
+					</Swiper>
+				</>
+			)}
 		</>
 	)
 }
